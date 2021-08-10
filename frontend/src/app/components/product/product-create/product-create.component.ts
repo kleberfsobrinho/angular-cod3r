6b289ea4e209029732e797869f335e25780bdffa
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-08-10 07:18:54 -0700

    Metodo Criar Produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -9,6 +10,11 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
@@ -16,6 +22,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct() : void {
+    this.productService.create(this.product).subscribe(() => {
+        this.productService.showMessage('Produto criado!');
+        this.router.navigate(['/products']);
+    })
     this.productService.showMessage('Produto criado!');
   }
 
